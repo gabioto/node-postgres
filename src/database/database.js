0a7056e3607bddbf20c754e30b0a38a8a3e02247
gabioto <dafne_rossi@hotmail.com>
2021-05-04 10:49:51 -0500

    creando models whit sequelize

---
+++ src/database/database.js
@@ -0,0 +1,20 @@
+import Sequelize from "sequelize";
+// parametros: bd, nombre_usuario,password, - host, tipo_db
+//seeuelize : [dialecto] permite conectarte adistitntas ds, psostgress maridb, myswl, sqlite
+//pool :para q tenga mutiples hilos de conexion
+export const sequelize = new Sequelize(
+    'postgres',
+    'postgres',
+    '75809581',
+    {
+        host:'localhost',
+        dialecto:'postgres',
+        pool:{
+            max:5,
+            min:0,
+            require:30000,
+            idle:10000
+        },
+        logging: false
+    }
+)
---
+++ src/models/Project.js
@@ -0,0 +1,31 @@
+import Sequelize from 'sequelize'
+import { sequelize } from '../database/database'
+import Task from './Task'
+//temgo la coenxion y algunas propiedades q viene de sequelize
+//necesito definir el modelo de datos de la bd (tbm se peude usar clases)
+const Project = sequelize.define('projects',{
+    id : {
+        type: Sequelize.INTEGER,
+        primaryKey :true
+    },
+    name:{
+        type: Sequelize.TEXT
+    },
+    priority:{
+        type: Sequelize.INTEGER
+    },
+    description:{
+        type:Sequelize.TEXT
+    },
+    deliverydate:{
+        type:Sequelize.DATE
+    }
+},{
+    timestamps:false   //siproblema pra fechas
+}
+);
+
+Project.hasMany(Task,{ foreingKey:'projectId', sourceKey:'id' }); //relacionamos projecto con las tareas ,a travez de la clave foranea
+Task.belongsTo(Project, {foreingKey: 'projectId', sourceKey:'id'});
+
+export default Project;
